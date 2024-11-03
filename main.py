2ba041eafd1f3f77f57789c0f0f08e0d370aaaca
vonot16 <rafaelfeliperm@gmail.com>
2024-11-03 15:37:25 -0300

    Clean Dataset Tool

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from glob import glob
+from os import getcwd, walk
+
+cwd = getcwd()
+original_dataset_dir = f"{cwd}\original_dataset\\"
+clean_dataset_dir = f"{cwd}\clean_dataset\\"
+datasets_name = []
+
+for (path, folders, files) in walk(original_dataset_dir):
+    datasets_name = files
+
+
+for dataset in datasets_name:
+    df = pd.read_csv(f"{original_dataset_dir}{dataset}", sep=',', header=0, index_col=None)
+    df.dropna(axis='index', how='any', inplace=True)
+
+    df.to_csv(f"{clean_dataset_dir}{dataset}", sep=',', index=False)
